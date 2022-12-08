41fa7e690c2b2986e71d7b284ca8c695dc83e46b
Clément <clem54210@outlook.fr>
2022-12-08 11:35:15 +0100

    Create fonc_score.ml
    Juste la fonction de score avec un test sur deux positions

---
+++ fonc_score.ml
@@ -0,0 +1,20 @@
+type position = {
+  i : int; (*position sur les lignes*)
+  j : int (*position sur les colonnes*)
+}
+
+let abs_val value = 
+  if value < 0 then
+    -value
+  else
+    value 
+
+let calcul_score = fun pos_cat pos_mouse ->
+  abs_val (pos_cat.i - pos_mouse.i) + abs_val (pos_cat.j - pos_mouse.j)
+
+
+let () =
+  let start_pos_mouse = {i = 0; j = 0} in 
+  let start_pos_cat = {i = 5; j = 5} in 
+  let result = calcul_score start_pos_cat start_pos_mouse in
+  Printf.printf "%d " result
