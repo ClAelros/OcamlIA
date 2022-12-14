0447df0c20b179a1c76cf3c992e1e9830577564a
totoo43 <bayonthms@gmail.com>
2022-12-14 14:30:02 +0100

    Changement calcul score

--- fonc_score.ml
+++ fonc_score.ml
@@ -11,10 +11,11 @@ let abs_val value =
 
 let calcul_score = fun pos_cat pos_mouse ->
   abs_val (pos_cat.i - pos_mouse.i) + abs_val (pos_cat.j - pos_mouse.j)
+  (* je propose (pos_cat.i - pos_mouse.i)**2+(pos_cat.j - pos_mouse.j)**2 (car c'est proportionnel à la distance)*)
 
 
 let () =
   let start_pos_mouse = {i = 0; j = 0} in 
   let start_pos_cat = {i = 5; j = 5} in 
   let result = calcul_score start_pos_cat start_pos_mouse in
-  Printf.printf "%d " result
+  Printf.printf "%d \n" result
